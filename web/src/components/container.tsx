3d2aadbff8c5ce8054df9953b00c498f6d05c154
Prithvi <prithvin19@gmail.com>
2022-05-04 23:42:53 +0530

    button

--- web/src/components/container.tsx
+++ web/src/components/container.tsx
@@ -4,6 +4,8 @@ export default function Container() {
   return (
     <div className='middle-container'>
         <div>My USDC: --</div>
+        <button className='order-button'>Create Order</button>
+        <div>No Orders</div>
         </div>
   )
 }
